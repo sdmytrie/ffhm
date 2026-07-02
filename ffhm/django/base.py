110c3750e4842d1a0f005b7664a8e36d8121e26f
Serge D <serge@dmytrienko.fr>
2026-07-02 21:47:08 +0200

    remove redis

--- ffhm/django/base.py
+++ ffhm/django/base.py
@@ -171,12 +171,12 @@ LOGIN_REDIRECT_URL = "/scoresheet"
 LOGOUT_REDIRECT_URL = "/scoresheet"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://redis:6379/1",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        },
-    }
-}
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         "LOCATION": "redis://redis:6379/1",
+#         "OPTIONS": {
+#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+#         },
+#     }
+# }
